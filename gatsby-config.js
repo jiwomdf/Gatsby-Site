d3d6052a6a669c96cf8905b5988bf0cb6a69f64d
jiwomdf <mdf.jiwo@outlook.com>
2021-07-04 12:52:16 +0700

    Finished Gatsby Tutorial Part 3

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,10 @@ module.exports = {
     siteUrl: "https://www.yourdomain.tld",
     title: "personalblog",
   },
-  plugins: ["gatsby-plugin-gatsby-cloud"],
+  plugins: [
+    "gatsby-plugin-gatsby-cloud",
+    "gatsby-plugin-gatsby-cloud",
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+  ],
 };
